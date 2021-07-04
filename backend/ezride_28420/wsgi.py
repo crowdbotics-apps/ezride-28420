6818c268c802154f770944236950ba7cd80c1323
Crowdbotics <team@crowdbotics.com>
2021-07-04 01:27:40 +0000

    Update models

--- backend/ezride_28420/wsgi.py
+++ backend/ezride_28420/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ezride_28420.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ezride_28420.settings")
 
 application = get_wsgi_application()
